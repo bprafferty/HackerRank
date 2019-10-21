f2e2d28ccfa064865971265071fd733bdaebf54e
bprafferty03 <bprafferty03@gmail.com>
2019-10-21 15:50:06 -0700

    solved min swaps in Python

---
+++ Data Structures/Arrays/Minimum Swaps 2/minSwaps.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    counter = 0
+
+    ph = [0]*(len(arr))
+
+    for index,val in enumerate(arr):
+            ph[val-1] = index
+
+    for i in range(len(arr)):
+        if (arr[i]) != i+1:
+            l = ph[i] 
+            arr[i],arr[l] = i+1,arr[i]
+            ph[i],ph[arr[l]-1] = i,l
+            counter += 1
+    return counter
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
